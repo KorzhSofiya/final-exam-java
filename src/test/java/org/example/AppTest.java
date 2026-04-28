5127fb6407fccf02675396266317c6acf425ed78
KorzhSofiya <ksofiko628@gmail.com>
2026-04-28 04:57:50 +0300

    Initial commit: setup

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class AppTest
+
+{
+
+}
